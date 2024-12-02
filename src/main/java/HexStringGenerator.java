53c6e10abea831223462b1a082c599a12f0a5a14
skeez <117862122+jkin2520@users.noreply.github.com>
2024-12-01 23:14:26 -0500

    Update HexStringGenerator.java
    adding documentation

--- src/main/java/HexStringGenerator.java
+++ src/main/java/HexStringGenerator.java
@@ -1,6 +1,17 @@
-import java.util.Random;
-
+/**
+ * Utility class for generating random hexadecimal strings.
+ * The generated string is formatted as follows:
+ * xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+ * where each "x" is a hexadecimal character (0-9, a-f).
+ */
 public class HexStringGenerator {
+
+    /**
+     * Generates a random 32-character hexadecimal string formatted as:
+     * xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.
+     *
+     * @return A formatted random hexadecimal string.
+     */
     public static String generateRandomHex() {
         Random random = new Random();
         StringBuilder hex = new StringBuilder();
@@ -8,11 +19,10 @@ public class HexStringGenerator {
             hex.append(Integer.toHexString(random.nextInt(16)));
         }
         return String.format("%s-%s-%s-%s-%s",
-            hex.substring(0, 8),
-            hex.substring(8, 12),
-            hex.substring(12, 16),
-            hex.substring(16, 20),
-            hex.substring(20));
+                hex.substring(0, 8),
+                hex.substring(8, 12),
+                hex.substring(12, 16),
+                hex.substring(16, 20),
+                hex.substring(20));
     }
 }
-
